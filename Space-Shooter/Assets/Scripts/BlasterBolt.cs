e0fd9115f38bdc6b8f0ebe41ad58f8bc7500e731
Liljan <marcus.t.lilja@gmail.com>
2016-09-11 19:16:09 +0200

    Player rapid fire.

---
+++ Space-Shooter/Assets/Scripts/BlasterBolt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlasterBolt : MonoBehaviour
+{
+    public Vector3 vel;
+    public Boundary bound;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += vel;
+
+        if (transform.position.y < bound.yMin || transform.position.y > bound.yMax)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Space-Shooter/Assets/Scripts/BlasterHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlasterHandler : MonoBehaviour
+{
+
+    public GameObject[] cannons;
+    private int currentCannon = 0;
+    public GameObject BlasterBoltPrefab;
+
+    public float coolDownTime, fireRate;
+    private float currentCooldownTime = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && currentCooldownTime <= 0f)
+        {
+            Fire();
+        }
+
+        currentCooldownTime -= Time.deltaTime;
+    }
+
+    void Fire()
+    {
+        Vector3 pos = cannons[currentCannon].transform.position;
+        Instantiate(BlasterBoltPrefab, pos, Quaternion.identity);
+
+        currentCooldownTime = coolDownTime;
+
+        ++currentCannon;
+        if (currentCannon > cannons.Length - 1)
+            currentCannon = 0;
+    }
+}
