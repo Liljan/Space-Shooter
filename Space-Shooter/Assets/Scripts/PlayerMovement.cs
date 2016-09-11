c4118d36be6ba27b364f863fc2a78d7690d7567a
Liljan <marcus.t.lilja@gmail.com>
2016-09-11 18:12:50 +0200

    Player movement.

--- Space-Shooter/Assets/Scripts/PlayerMovement.cs
+++ Space-Shooter/Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Rigidbody2D rb2d;
+    private float moveH, moveV;
+    private Vector3 movement;
+
+    public Boundary boundary;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GameObject.FindObjectOfType<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveH, moveV);
+        rb2d.velocity = movement * speed;
+
+        rb2d.position = new Vector3(
+            Mathf.Clamp(rb2d.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb2d.position.y, boundary.yMin, boundary.yMax));
+
+        transform.rotation = Quaternion.Euler(0f, rb2d.velocity.normalized.x * -tilt, 0f);
+    }
 }
