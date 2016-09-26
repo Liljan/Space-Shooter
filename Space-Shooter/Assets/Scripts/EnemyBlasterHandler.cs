0d924327ebaafb9df5b07daf72c3e97588f10ed0
Liljan <marcus.t.lilja@gmail.com>
2016-09-26 11:06:43 +0200

    Added enemy blaster handler class.

---
+++ Space-Shooter/Assets/Scripts/EnemyBlasterHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBlasterHandler : MonoBehaviour
+{
+    public GameObject[] cannons;
+
+    private int currentCannon = 0;
+    public GameObject BlasterBoltPrefab;
+
+    public float fireRate = 1;
+    private float timeToFire;
+
+    public float blasterSpeed;
+
+    // audio
+    private AudioManager am;
+    public AudioClip SFX_blast;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timeToFire = Random.Range(5f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeToFire <= 0.0f)
+        {
+            Fire();
+            Fire();
+
+            timeToFire = Random.Range(5f, 10f);
+        }
+
+        timeToFire -= Time.deltaTime;
+
+    }
+
+    void Fire()
+    {
+        Vector3 pos = cannons[currentCannon].transform.position;
+
+        GameObject obj = Instantiate(BlasterBoltPrefab, pos, Quaternion.identity) as GameObject;
+        obj.GetComponent<BlasterBolt>().Init(new Vector3(0.0f, -blasterSpeed, 0.0f));
+
+        ++currentCannon;
+        if (currentCannon > cannons.Length - 1)
+            currentCannon = 0;
+    }
+}
