4a9a40d5f1d0b4f7799e73f9d211f273973611e6
Liljan <marcus.t.lilja@gmail.com>
2016-10-02 12:28:40 +0200

    Missiles follows the closest enemy.

--- Space-Shooter/Assets/Scripts/Missile.cs
+++ Space-Shooter/Assets/Scripts/Missile.cs
@@ -2,29 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Missile : MonoBehaviour {
-
+public class Missile : MonoBehaviour
+{
     private Rigidbody2D rb2d;
-
     private Transform target;
 
-    public Vector2 speed;
+    public float speed;
 
 
     public void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        //rb2d = GetComponent<Rigidbody2D>();
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         target = FindTarget();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+       // rb2d.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 dir = target.position - transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90.0f;
+
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+        }
+
+    }
 
     private Transform FindTarget()
     {
@@ -39,12 +49,12 @@ public class Missile : MonoBehaviour {
         int idxClosest = 0;
         Vector3 missilePos = this.transform.position;
 
-        for(int i = 0; i < enemies.Length; ++i)
+        for (int i = 0; i < enemies.Length; ++i)
         {
             Vector3 v1 = enemies[idxClosest].transform.position - missilePos;
             Vector3 v2 = enemies[i].transform.position - missilePos;
 
-            if(v2.sqrMagnitude < v1.sqrMagnitude)
+            if (v2.sqrMagnitude < v1.sqrMagnitude)
             {
                 idxClosest = i;
             }
@@ -52,6 +62,5 @@ public class Missile : MonoBehaviour {
 
         Debug.Log(enemies[idxClosest]);
         return enemies[idxClosest].transform;
-        
     }
 }
