388c81b7a8029b7dc7417b46bb014dd7bd25975f
Liljan <marcus.t.lilja@gmail.com>
2016-10-09 21:16:51 +0200

    Tooltip messages for selecting planets.

---
+++ Space-Shooter/Assets/Scripts/MenuToolTip.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuToolTip : MonoBehaviour {
+
+    public Text tipTxt;
+    public string text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetToolTip()
+    {
+        tipTxt.text = text;
+    }
+
+
+}
