75a25f97912ecd76c3334939114cdfbf2767f916
Liljan <marcus.t.lilja@gmail.com>
2016-10-19 17:09:35 +0200

    Asteroid prefab.

---
+++ Space-Shooter/Assets/Scripts/In-Game/Enemy/AsteroidMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidMovement : MonoBehaviour
+{
+    private float rotation = 0.0f;
+    public float MAX_ANGULAR_VELOCITY;
+    private float angularVelocity;
+
+    public void Start()
+    {
+        angularVelocity = Random.Range(-MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY);
+        angularVelocity *= Mathf.Deg2Rad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, rotation);
+        rotation += Time.deltaTime * angularVelocity;
+    }
+}
