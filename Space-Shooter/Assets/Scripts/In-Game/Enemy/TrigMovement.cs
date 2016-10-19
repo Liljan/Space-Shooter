aaea9e86854627db3b80f5b3be12820ed86cbc6d
Liljan <marcus.t.lilja@gmail.com>
2016-10-19 20:49:23 +0200

    Correct starting position for tie interceptors.

--- Space-Shooter/Assets/Scripts/In-Game/Enemy/TrigMovement.cs
+++ Space-Shooter/Assets/Scripts/In-Game/Enemy/TrigMovement.cs
@@ -1,16 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrigMovement : EnemyMovement
+public class TrigMovement : MonoBehaviour
 {
     public float magnitude;
     private float phase = 0.0f;
     public float angularVelocity;
 
+    public Vector2 vel;
+    private Rigidbody2D rb2d;
+    private float centerX;
+
+    // Use this for initialization
+    public void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = vel;
+        centerX = rb2d.position.x;
+    }
+
     void Update()
     {
         Vector3 pos = rb2d.position;
-        pos.x = magnitude * Mathf.Cos(phase * Mathf.Deg2Rad);
+        pos.x = centerX + magnitude * Mathf.Cos(phase * Mathf.Deg2Rad);
         rb2d.position = pos;
 
         Debug.Log(pos);
